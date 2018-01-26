2f9cde4bae3324e606711548e87d266c232ec161
jonatahessa <jonata.hessa@gmail.com>
2018-01-26 13:37:42 -0200

    First commit

---
+++ PazzaPizzaria/src/main/webapp/_resources/domPaginaInicial.js
@@ -0,0 +1,34 @@
+function main() {
+  document.querySelector("#buttonhome").addEventListener("click", home);
+  document.querySelector("#buttonpromocoes").addEventListener("click", promocao);
+  document.querySelector("#buttonnossacasa").addEventListener("click", nossacasa);
+  document.querySelector("#buttoncardapio").addEventListener("click", cardapio);
+  document.querySelector("#buttonlocalizacao").addEventListener("click", localizacao);
+  document.querySelector("#buttoncontato").addEventListener("click", scrollcontato);
+}
+
+function home() {
+  window.scrollTo(0,0);
+}
+
+function nossacasa() {
+  document.querySelector("#nossacasa").scrollIntoView();
+}
+
+function promocao() {
+  document.querySelector("#sessaopromocoes").scrollIntoView();
+}
+
+function cardapio() {
+  document.querySelector("#sessaocardapio").scrollIntoView();
+}
+
+function localizacao() {
+  document.querySelector("#sessaolocalizacao").scrollIntoView();
+}
+
+function scrollcontato() {
+  document.querySelector("#sessaocontato").scrollIntoView();
+}
+
+window.addEventListener("load", main);
