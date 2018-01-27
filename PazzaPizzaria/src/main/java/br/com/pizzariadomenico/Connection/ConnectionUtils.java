bb864f024a6aadf42a8a080d57d49739fa02eb5a
jonatahessa <jonata.hessa@gmail.com>
2018-01-26 22:17:42 -0200

    Exclusão de senha

--- PazzaPizzaria/src/main/java/br/com/pizzariadomenico/Connection/ConnectionUtils.java
+++ PazzaPizzaria/src/main/java/br/com/pizzariadomenico/Connection/ConnectionUtils.java
@@ -26,7 +26,7 @@ public class ConnectionUtils {
             String mydatabase = "pazzapizza"; // Nome do banco de dados
             String url = "jdbc:mysql://" + serveName + "/" + mydatabase;
             String username = "pazza01"; // Nome de usuario
-            String password = "br@sil20"; // Senha de acesso
+            String password = ""; // Senha de acesso
             connection = DriverManager.getConnection(url, username, password);
             
             //Teste de conexao
